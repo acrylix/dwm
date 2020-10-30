1eef162225b71e37ec8fdbc5cfdc4e8f6fb71041
Michael Yu <1619025+acrylix@users.noreply.github.com>
2020-10-29 20:14:05 -0400

    update colors

--- config.def.h
+++ config.def.h
@@ -17,10 +17,11 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_yellow[]		= "#ffcd44";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray1, col_yellow,  col_yellow  },
 };
 
 /* tagging */
@@ -136,7 +137,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_BackSpace, quit,        {0} },
+	{ MODKEY|SUPERKEY|ShiftMask,	XK_BackSpace, quit,        {0} },
 };
 
 /* button definitions */
