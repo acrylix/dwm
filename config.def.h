4424b57f54f1873b1ffdbbf22a10247307d13046
Michael Yu <1619025+acrylix@users.noreply.github.com>
2021-02-01 01:22:33 -0500

    sel color change

--- config.def.h
+++ config.def.h
@@ -19,10 +19,11 @@ static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char col_yellow[]		= "#ffcd44";
+static const char col_olive[]		= "#b4c248";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray1, col_yellow,  col_yellow  },
+	[SchemeSel]  = { col_gray1, col_olive,  col_olive  },
 };
 
 /* tagging */
